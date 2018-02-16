4c302a80dc26869e54d1bfcfba6c26f9493746f9
maccode421 <maccavalli@yahoo.com>
2018-02-16 17:40:54 -0500

    add js for login

---
+++ script/main.js
@@ -0,0 +1,11 @@
+// function to check email & password
+function check(form) {
+  // the following code checkes whether the entered email and password are matching
+  if (form.email.value == "myemail" && form.password.value == "mypassword") {
+    // opens the target page while email & password matches
+    window.open('target.html')
+  } else {
+    // displays error message
+    alert("Error Password or Email")
+  }
+}
