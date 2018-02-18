2018c56bba2df671c2d5afc4b85946fdc07bc136
maccode421 <maccavalli@yahoo.com>
2018-02-17 21:09:12 -0500

    rename file and add/update login and create file

--- script/main.js
+++ script/main.js
@@ -9,3 +9,13 @@ function check(form) {
     alert("Error Password or Email")
   }
 }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
